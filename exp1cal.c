138d887b63fe3fcbbe7504e56cf522637cf38eae
Anushka Chauhan <ac3671@srmist.edu.in>
2024-05-13 05:36:53 +0530

    Add experiment 1, 3, 4, 6, 7, 8, 9, 10, 11, 12

---
+++ exp1cal.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    // Prompt the user to enter the operator (+, -, *, /)
+    printf("Enter operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    // Prompt the user to enter two numbers
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    // Perform the calculation based on the operator
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("%.2lf + %.2lf = %.2lf", num1, num2, result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("%.2lf - %.2lf = %.2lf", num1, num2, result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("%.2lf * %.2lf = %.2lf", num1, num2, result);
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("%.2lf / %.2lf = %.2lf", num1, num2, result);
+            } else {
+                printf("Error! Division by zero.");
+            }
+            break;
+        default:
+            printf("Error! Invalid operator.");
+    }
+
+    return 0;
+}
---
+++ exp4calculator.y
@@ -0,0 +1,59 @@
+%{
+#include <stdio.h>
+#include <stdlib.h>
+%}
+
+%token NUMBER
+%token PLUS MINUS TIMES DIVIDE
+%left PLUS MINUS
+%left TIMES DIVIDE
+
+%%
+
+calc: expr { printf("Result: %d\n", $1); }
+    ;
+
+expr: expr PLUS expr  { $$ = $1 + $3; }
+    | expr MINUS expr { $$ = $1 - $3; }
+    | expr TIMES expr { $$ = $1 * $3; }
+    | expr DIVIDE expr { 
+        if ($3 != 0) {
+            $$ = $1 / $3; 
+        } else {
+            fprintf(stderr, "Error: Division by zero\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    | NUMBER { $$ = $1; }
+    | '(' expr ')' { $$ = $2; }
+    ;
+
+%%
+
+int main() {
+    yyparse();
+    return 0;
+}
+
+int yyerror(const char *msg) {
+    fprintf(stderr, "Error: %s\n", msg);
+    return 1;
+}
+
+int yylex() {
+    int c = getchar();
+    if (c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')') {
+        return c;
+    } else if (c == '\n') {
+        return 0;
+    } else if (c >= '0' && c <= '9') {
+        ungetc(c, stdin);
+        int value;
+        scanf("%d", &value);
+        return NUMBER;
+    } else if (c == EOF) {
+        return 0;
+    } else {
+        return -1; // Error
+    }
+}
